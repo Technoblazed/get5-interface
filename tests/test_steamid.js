8e78ba72abc2c4c8ed5fae07722a0da2a7487ec3
Anthony <anthony@tech-no.me>
2018-02-28 01:26:35 +0000

    cleanup steamid tests a little

--- tests/test_steamid.js
+++ tests/test_steamid.js
@@ -5,25 +5,26 @@ const steamid = require('../lib/steamid');
 
 describe('steamid', () => {
   describe('authToSteam64', () => {
-    const testValues = {
-      'STEAM_0:0:90996256': '76561198142258240',
-      'STEAM_1:0:90996256': '76561198142258240',
-      '76561198142258240': '76561198142258240',
-      '1:0:90996256': '76561198142258240',
-      '[U:1:181992512]': '76561198142258240',
-      'steamcommunity.com/profiles/76561198142258240': '76561198142258240',
-      'http://steamcommunity.com/profiles/76561198142258240': '76561198142258240',
-      'http://steamcommunity.com/profiles/76561198142258240/': '76561198142258240',
-      'http://steamcommunity.com/id/Technoblazed': '76561198142258240',
-      'technoblazed': '76561198142258240'
-    };
+    const target = '76561198142258240';
+    const testValues = [
+      'STEAM_0:0:90996256',
+      'STEAM_1:0:90996256',
+      '76561198142258240',
+      '1:0:90996256',
+      '[U:1:181992512]',
+      'steamcommunity.com/profiles/76561198142258240',
+      'http://steamcommunity.com/profiles/76561198142258240',
+      'http://steamcommunity.com/profiles/76561198142258240/',
+      'http://steamcommunity.com/id/Technoblazed',
+      'technoblazed'
+    ];
 
-    _.forOwn(testValues, (value, key) => {
-      it(`"${key}" should return "${value}"`, async() => {
+    _.forEach(testValues, (key) => {
+      it(`"${key}" should return "${target}"`, async() => {
         const [success, actual] = await steamid.authToSteam64(key);
 
         expect(success).to.be.true;
-        expect(actual).to.be.equal(value);
+        expect(actual).to.be.equal(target);
       });
     });
   });
